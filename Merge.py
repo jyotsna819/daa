ece9d49511dd6c72368f19d165041be05cb8d804
jyotsna819 <88374847+jyotsna819@users.noreply.github.com>
2025-05-15 14:12:02 +0530

    Create Merge.py

---
+++ Merge.py
@@ -0,0 +1,42 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        # Find the middle point
+        mid = len(arr) // 2
+
+        # Divide the array elements into 2 halves
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        # Recursively sort both halves
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        # Merge the sorted halves
+        i = j = k = 0
+
+        # Copy data to temp arrays left_half[] and right_half[]
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+# Taking input from user
+arr = list(map(int, input("Enter the numbers separated by space: ").split()))
+
+merge_sort(arr)
+print("Sorted array:", arr)
